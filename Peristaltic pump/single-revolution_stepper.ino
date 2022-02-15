955d1184767b9978ad51f68716cc52a3305530de
René Cartaya <52467158+renecartaya@users.noreply.github.com>
2022-02-15 13:25:13 +0100

    Update single-revolution_stepper.ino

--- Peristaltic pump/single-revolution_stepper.ino
+++ Peristaltic pump/single-revolution_stepper.ino
@@ -1,19 +1,5 @@
 
-/*
- Stepper Motor Control - one revolution
 
- This program drives a unipolar or bipolar stepper motor.
- The motor is attached to digital pins 8 - 11 of the Arduino.
-
- The motor should revolve one revolution in one direction, then
- one revolution in the other direction.
-
-
- Created 11 Mar. 2007
- Modified 30 Nov. 2009
- by Tom Igoe
-
- */
 
 #include <Stepper.h>
 
