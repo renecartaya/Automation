2c6192d801efca11a37063dba0a50f6e0ebf2d21
René Cartaya <52467158+renecartaya@users.noreply.github.com>
2022-02-16 01:19:53 +0100

    Add files via upload

---
+++ GUI_Python_Arduino_Firmware_-_III/GUI_Python_Arduino_Firmware_-_III.ino
@@ -0,0 +1,43 @@
+char userInput;
+
+void setup()
+{
+  Serial.begin(9600);                        //  setup serial
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+int getDelayTime()
+{
+  String blinkTimeString;
+  int blinkTimeInt = 0;
+  delay(2000); // Need to wait for pyserial send time 
+  blinkTimeString = Serial.readString();
+  blinkTimeInt = blinkTimeString.toInt();
+  return blinkTimeInt;
+} // Get Delay Time function
+
+void loop(){
+
+if(Serial.available()> 0){ 
+    
+    userInput = Serial.read();               // read user input
+      
+      if(userInput == 'H'){                
+        digitalWrite(LED_BUILTIN, HIGH); 
+      }
+      
+      if(userInput == 'L'){
+       digitalWrite(LED_BUILTIN, LOW);         
+      }
+      if(userInput == 'h'){
+        int delayTime = getDelayTime();
+        for(int i =0;i<10;i++){
+          digitalWrite(LED_BUILTIN,LOW);
+          delay(delayTime);
+          digitalWrite(LED_BUILTIN,HIGH);
+          delay(delayTime);
+         }// Blink for loop
+      digitalWrite(LED_BUILTIN, LOW); 
+      }
+  } // Serial.available
+} // Void Loop
