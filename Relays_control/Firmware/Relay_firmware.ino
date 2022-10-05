9e35a6bace4ec902e6e40dbc43eb7488ea1784be
René Cartaya <52467158+renecartaya@users.noreply.github.com>
2022-10-05 13:10:21 +0200

    Add files via upload

--- Relays_control/Firmware/Relay_firmware.ino
+++ Relays_control/Firmware/Relay_firmware.ino
@@ -8,18 +8,32 @@
 */
 
 // Defining
-const int RELAY_PIN = 3;  // defining the arduino pin connects to connected to the pin IN of the relay
 
-// the setup function runs once when you press reset or power the board
+char user_In; // User serial input
+const int relay_pin = 3;  // defining the arduino pin connects to connected to the pin IN of the relay
+
+
 void setup() {
-  // initialize digital pin as an output.
-  pinMode(RELAY_PIN, OUTPUT);
+  Serial.begin(9600); // force to do actions just under serial signals commands
+  digitalWrite (relay_pin, LOW); //initially set to low, it could also depends of the pins connected on the relay
+  pinMode(relay_pin, OUTPUT); // initialize digital pin as an output.
 }
 
 // the loop function runs over and over again forever
-void loop() {
-  digitalWrite(RELAY_PIN, HIGH);
-  delay(1000);
-  digitalWrite(RELAY_PIN, LOW);
-  delay(1000);
+void loop()
+{
+
+  while (Serial.available())
+  {
+    user_In = Serial.read();
+  }
+
+  if (user_In == 'H')
+  digitalWrite(relay_pin, HIGH);   // turn the LED on (HIGH is the voltage level)
+
+  
+  if (user_In == 'L')
+  
+  digitalWrite(relay_pin, LOW);    // turn the LED off by making the voltage LOW
+
 }
