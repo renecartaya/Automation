59c4108c716cf60e47e449ddb28b76f70b2afc87
René Cartaya <52467158+renecartaya@users.noreply.github.com>
2022-10-06 12:38:43 +0200

    Add files via upload

--- Relays_control/Firmware/Relay_firmware.ino
+++ Relays_control/Firmware/Relay_firmware.ino
@@ -1,22 +1,31 @@
 /* Author: René Cartaya
    Date: September 2022
    Goal: Computer control of a relay of 5V
-   Description:  
-    S: pin 3
-    +: 5V
-    -: GND
+   Description: Firmware for the control of a module of 4 relays
 */
 
 // Defining
 
 char user_In; // User serial input
-const int relay_pin = 3;  // defining the arduino pin connects to connected to the pin IN of the relay
-
+const int relay_pin_1 = 3;  // defining the arduino pin connects to connected to the pin IN of the relays
+const int relay_pin_2 = 5;
+const int relay_pin_3 = 6;
+const int relay_pin_4 = 9;
 
 void setup() {
   Serial.begin(9600); // force to do actions just under serial signals commands
-  digitalWrite (relay_pin, LOW); //initially set to low, it could also depends of the pins connected on the relay
-  pinMode(relay_pin, OUTPUT); // initialize digital pin as an output.
+  
+  digitalWrite (relay_pin_1, LOW); //initially set to low, it could also depends of the pins connected on the relay
+  pinMode(relay_pin_1, OUTPUT); // initialize digital pin as an output.
+  
+  digitalWrite (relay_pin_2, LOW); //initially set to low, it could also depends of the pins connected on the relay
+  pinMode(relay_pin_2, OUTPUT); // initialize digital pin as an output.
+
+  digitalWrite (relay_pin_3, LOW); //initially set to low, it could also depends of the pins connected on the relay
+  pinMode(relay_pin_3, OUTPUT); // initialize digital pin as an output.
+
+  digitalWrite (relay_pin_4, LOW); //initially set to low, it could also depends of the pins connected on the relay
+  pinMode(relay_pin_4, OUTPUT); // initialize digital pin as an output.
 }
 
 // the loop function runs over and over again forever
@@ -28,12 +37,35 @@ void loop()
     user_In = Serial.read();
   }
 
-  if (user_In == 'H')
-  digitalWrite(relay_pin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  if (user_In == 'A')
+  digitalWrite(relay_pin_1, HIGH);   // turn the LED on (HIGH is the voltage level)
 
   
-  if (user_In == 'L')
+  if (user_In == 'B')
+  digitalWrite(relay_pin_1, LOW);    // turn the LED off by making the voltage LOW
+
+ 
+  if (user_In == 'C')
+  digitalWrite(relay_pin_2, HIGH);   // turn the LED on (HIGH is the voltage level)
+
   
-  digitalWrite(relay_pin, LOW);    // turn the LED off by making the voltage LOW
+  if (user_In == 'D')
+  digitalWrite(relay_pin_2, LOW);    // turn the LED off by making the voltage LOW
+  
+
+  if (user_In == 'E')
+  digitalWrite(relay_pin_3, HIGH);   // turn the LED on (HIGH is the voltage level)
+
+  
+  if (user_In == 'F')
+  digitalWrite(relay_pin_3, LOW);    // turn the LED off by making the voltage LOW
+  
+
+  if (user_In == 'G')
+  digitalWrite(relay_pin_4, HIGH);   // turn the LED on (HIGH is the voltage level)
+
+  
+  if (user_In == 'H')
+  digitalWrite(relay_pin_4, LOW);    // turn the LED off by making the voltage LOW
 
 }
